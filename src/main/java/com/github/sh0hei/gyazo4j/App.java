fb41453d0d6f8bdc75a1efd53292797b89fa4512
sh0hei <vividbit@gmail.com>
2014-07-31 17:34:32 +0900

    first commit

---
+++ src/main/java/com/github/sh0hei/gyazo4j/App.java
@@ -0,0 +1,13 @@
+package com.github.sh0hei.gyazo4j;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
